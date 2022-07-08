cb75a0fadb9b1d533baea3e12d7461b43b9f6c66
abugraokkali <abugraokkali@outlook.com>
2022-07-08 13:51:16 +0300

    first commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+
+    public float cameraDistance = 2.0f;
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+        transform.position = target.transform.position - target.transform.forward * cameraDistance;
+        transform.LookAt (target.transform.position);
+        transform.position = new Vector3 (transform.position.x, transform.position.y + 2.5f, transform.position.z);
+  
+        //transform.rotation = target.rotation;
+
+    }
+}
